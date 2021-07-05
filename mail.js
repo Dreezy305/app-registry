bfe2e4f0761164fec42099b34c191086ec90c1b3
Dreezy305 <bankoleidris@gmail.com>
2021-07-05 14:01:53 +0000

    working on mailer

--- mail.js
+++ mail.js
@@ -5,8 +5,8 @@ const mailGun = require("nodemailer-mailgun-transport");
 
 const auth = {
   auth: {
-    api_key: "",
-    domain: "",
+    api_key: "key-8f841f1090c759f815a8f65807badd88-1f1bd6a9-e48e58ce",
+    domain: "sandbox81ca0f9721d440868794e313bb009dbd.mailgun.org",
   },
 };
 
