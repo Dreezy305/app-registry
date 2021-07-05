0d6319e93868448b352a3054b3f1679cac98b83f
Dreezy305 <bankoleidris@gmail.com>
2021-07-05 11:27:20 +0000

    working on mailer

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const path = require("path");
 
 const PORT = 8070;
 
+// data parsing
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.post("/email", (req, res) => {
+  // send email here
+  res.join({ message: "message received" });
+});
+
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "form.html"));
 });
