fbba7138e69aad92f7a5c37ac9c505ed6e003a81
Dreezy305 <bankoleidris@gmail.com>
2021-07-14 15:47:29 +0000

    serving static files

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const PORT = 8080;
 // data parsing
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.post("/email", (req, res) => {
   // send email here
@@ -35,8 +36,8 @@ app.post("/email", (req, res) => {
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "form.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "form.html"));
+// });
 
 app.listen(PORT, () => console.log("listening on port, ", PORT));
